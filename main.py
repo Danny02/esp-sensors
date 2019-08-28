0972c569b9f04ab885f7e2713c639dabdc740372
Daniel Heinrich <dannynullzwo@gmail.com>
2019-08-28 15:10:59 +0200

    test meassurment

--- main.py
+++ main.py
@@ -3,24 +3,23 @@ import ujson
 from main import sensors
 from main.ota_updater import OTAUpdater
 
+import machine
+
 
 def download_and_install_update_if_available(config_data):
 	if 'wifi' in config_data:
-		o = OTAUpdater('https://github.com/Danny02/esp-sensors')
-		o.using_network(config_data['wifi']['ssid'], config_data['wifi']['password'])
-		o.check_for_update_to_install_during_next_reboot()
+		o = OTAUpdater(config_data['repo'])
 		o.download_and_install_update_if_available(config_data['wifi']['ssid'], config_data['wifi']['password'])
 	else:
 		print('No WIFI configured, skipping updates check')
 
 
 def start(config_data):
-	sensors.main()
+	sensors.main(config_data)
 
 def boot():
 	f = open('config.json')
 	config_data = ujson.load(f)
-
 	download_and_install_update_if_available(config_data)
 	start(config_data)
 
--- main/sensors.py
+++ main/sensors.py
@@ -1,7 +1,32 @@
 import machine
 import time
+import dht
+
+from main.ota_updater import OTAUpdater
 
 
-def main():
-	time.sleep(2)
-	print("hallo welt")
+def main(config_data):
+	OTAUpdater.using_network(config_data['wifi']['ssid'], config_data['wifi']['password'])
+
+	m = meassureDht(config_data)
+	print(m)
+
+ 	checkForUpdates(config_data)
+
+	machine.deepsleep(config_data['sleep-for'])
+
+
+def meassureDht(config_data):
+	d = dht.DHT22(machine.Pin(4))
+	d.measure()
+	t = d.temperature()
+	h = d.humidity()
+	return """"humidity,id=dev value={humidity}
+               temperature,id=dev value={temperature}""".format(humidity = h, temperature = t)
+
+
+def checkForUpdates(config_data):
+	o = OTAUpdater(config_data['repo'])
+	o.check_for_update_to_install_during_next_reboot()
+
+
