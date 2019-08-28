b9104003013e6f5fe1fd816a1a648f81b5868020
Daniel Heinrich <dannynullzwo@gmail.com>
2019-08-28 13:24:20 +0200

    deploy first version

---
+++ main.py
@@ -0,0 +1,26 @@
+import ujson
+
+from main import sensors
+from main.ota_updater import OTAUpdater
+
+
+def download_and_install_update_if_available(config_data):
+	if 'wifi' in config_data:
+		o = OTAUpdater('https://github.com/Danny02/esp-sensors')
+		o.download_and_install_update_if_available(config_data['wifi']['ssid'], config_data['wifi']['password'])
+	else:
+		print('No WIFI configured, skipping updates check')
+
+
+def start(config_data):
+	sensors.main()
+
+def boot():
+	f = open('config.json')
+	config_data = ujson.load(f)
+
+	download_and_install_update_if_available(config_data)
+	start(config_data)
+
+
+boot()
---
+++ main/sensors.py
@@ -0,0 +1,7 @@
+import machine
+import time
+
+
+def main():
+	time.sleep(2)
+	print("hallo welt")
